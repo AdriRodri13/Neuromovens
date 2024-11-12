e7c41bde26a052ead34bf1a09e7f62bcd8bbf4e6
dania <adridev88@gmail.com>
2024-11-12 09:21:50 +0100

    comit

--- www.neuromovens.com/assets/Compartido/header.php
+++ www.neuromovens.com/assets/Compartido/header.php
@@ -7,7 +7,7 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NeuroMovens - Contacto</title>
+    <title>NeuroMovens - '.$current_page. '</title>
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
