0342b306f17cc5e08ec22214c40ea452650e553b
Adri <152196347+AdriRodri13@users.noreply.github.com>
2025-05-23 10:34:12 +0200

    Neuromovens 2.0 cookies

--- www.neuromovens.com/assets/Entidades/Entidad.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Entidades;
-
-interface Entidad
-{
-
-}
--- www.neuromovens.com/assets/Vistas/cerraSesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['usuario'] = false;
-$_SESSION['nombre_usuario'] = null;
-header("location:../../index.php");
